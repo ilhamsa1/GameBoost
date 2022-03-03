861c38e815477f36e96182410fb48e10d5f42269
ilhamsa1 <muhammad.ilhamsa1@gmail.com>
2022-03-03 23:01:02 +0700

    Add Audio

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,15 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     Rigidbody rb;
+
+    AudioSource m_MyAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        m_MyAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +31,14 @@ public class Movement : MonoBehaviour
         {
             // rb.AddRelativeForce(0, 1, 0);
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!m_MyAudioSource.isPlaying)
+            {
+                m_MyAudioSource.Play();
+            }
+        }
+        else
+        {
+            m_MyAudioSource.Stop();
         }
     }
 
